e2f5d2fe4d1da03a0ef9b6d9a9b20c858be940dc
Kolja Sam <github@koljapluemer.com>
2026-05-15 10:53:36 +0200

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [tailwindcss(), vue()],
@@ -8,8 +9,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  test: {
-    environment: 'jsdom',
   },
 })
